7853fd262cdb73e54d4e7ec0b42f35a9a5ce4d11
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2019-03-02 16:27:21 +0900

    Modify printing code

--- examples/simpcomp.rs
+++ examples/simpcomp.rs
@@ -3,7 +3,6 @@ use std::io::BufReader;
 use teia::reader::simpcomp;
 use teia::z2reduce::Z2ColumnReduce;
 use teia::z2vector::{Z2Chain, Z2VectorVec, Z2VectorIter};
-use teia::traits::*;
 use teia::pair::Pair;
 
 fn main() {
@@ -11,18 +10,15 @@ fn main() {
 
     let comp = simpcomp::read_simpcomp_text(BufReader::new(file)).unwrap();
 
-    println!("# Complex");
-    for simp in comp.basis.iter() {
-        println!("{:?}", simp);
-    }
-
     let reduce = Z2ColumnReduce::<Z2Chain<Z2VectorVec>>
             ::from_complex_with(&comp, |index, chain| Z2Chain::new(index, chain)).unwrap();
 
+    println!("# persistence generator");
     for (pers, chain) in Pair::new(&reduce, reduce.cycles()) {
-        println!("{:?}", pers);
+        print!("{:?} [", pers);
         for x in chain.chain.iter() {
-            println!("  {}", comp.basis[*x]);
+            print!("{},", comp.basis[*x]);
         }
+        println!("]");
     }
 }
